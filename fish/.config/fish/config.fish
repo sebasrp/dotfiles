c1e5692dd7246dcf1e6c60a331660d8868afaef8
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2024-05-07 23:39:03 +0800

    remove fish greeting message

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 # Add HomeBrew's bin directory to path so you can use HomeBrew's binaries
 # Fish uses `fish_add_path` instead of `export PATH` modify $PATH.
 fish_add_path /opt/homebrew/bin/
+set fish_greeting
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
