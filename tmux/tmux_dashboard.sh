d0e4a3640cd1b31a30f68146aeed45c3a4d5b630
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2024-05-07 23:45:39 +0800

    fix tmux bash script after the tmux latest upgrade

--- tmux/tmux_dashboard.sh
+++ tmux/tmux_dashboard.sh
@@ -6,16 +6,11 @@ lines="$(tput lines)"
 columns="$(tput cols)"
 
 tmux -2 new-session -d -x "$columns" -y "$lines" -s "$session"
-tmux new-window -a -t "$window" || exit
+tmux split-window -v -l 85%
+tmux select-pane -t 0
+tmux split-window -h -l 30% 'tty-clock -c'
 
-tmux split-window -t "$window" -v -p 85
-tmux split-window -t "$window" -v -p 30
-tmux split-window -t $session:1.0 -h -p 30
-
-# display clock
-tmux send-keys -t $session:1.1 'tty-clock -c' Enter
-
-tmux select-window -t "$window"
+tmux select-pane -t 2
 
 # Attach to session
 tmux -2 attach-session -t "$session"
