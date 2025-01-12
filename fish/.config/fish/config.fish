67728d1d7553bd5907e039e7ef83c0c5527805d2
Sebastian Rodriguez <331558+sebasrp@users.noreply.github.com>
2025-01-12 21:00:23 +0800

    rbvenv fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,3 +6,6 @@ set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Added by `rbenv init` on Thu Jul 25 12:21:09 +08 2024
+status --is-interactive; and rbenv init - --no-rehash fish | source
